2317dcba698193a56642a6122783c5cf6e2a65fd
wawanphd <gunawanwawan15@gmail.com>
2023-11-27 08:58:18 +0700

    update table

--- manage.php
+++ manage.php
@@ -5,7 +5,29 @@
         </div>
         <div class="card-body">
             <h5 class="card-title">Manage Artikel</h5>
-            <p class="card-text">.</p>
+             <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Judul</th>
+                        <th>Isi</th>
+                        <th>Kategori</th>
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>hapus | edit</td>
+                    </tr>
+                </tbody>
+             </table>   
+
+
+
         </div>
     </div>
 </div>
