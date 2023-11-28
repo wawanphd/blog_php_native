ee36b87a0b09d60b405e08744642bf8dba65ac24
wawanphd <gunawanwawan15@gmail.com>
2023-11-28 08:37:50 +0700

    SQL

---
+++ data.sql
@@ -0,0 +1,10 @@
+-- auto-generated definition
+create table tbl_blog
+(
+    id       int auto_increment primary key,
+    judul    varchar(100) not null,
+    isi      varchar(255) not null,
+    kategori varchar(50)  not null
+);
+
+
